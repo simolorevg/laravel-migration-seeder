a750783584d959c8b8261068af814d0ed1a85c72
simolorevg <simoneviaggi@libero.it>
2023-05-25 15:53:30 +0200

    Connected and i can show in the page the datas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,1 +1,11 @@
 @extends('layouts.app')
+<h1 class="text-center">
+    Treni
+</h1>
+<div class="container d-flex justify-content-center align-items-center">
+    <ul>
+        @foreach ($trains as $item)
+            <li>{{ $item->agency }}</li>
+        @endforeach
+    </ul>
+</div>
